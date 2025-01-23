7492e3ba6005c0ab943ee0673ff725adad3b2b1c
Thomas Weaverson <thomasweaverson@gmail.com>
2025-01-23 11:11:22 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // пути к файлам
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
