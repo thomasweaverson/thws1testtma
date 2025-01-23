2270c880b6c479fa8409deb8b2b51beccebaa359
Thomas Weaverson <thomasweaverson@gmail.com>
2025-01-23 13:37:18 +0300

    0.3 test app with tg api

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,34 @@
 import React, { useEffect, useState } from "react";
-// import UserInfoButton from "./components/user-info-button/user-info-button";
-// import Card from "./components/card/card";
+import UserInfoButton from "./components/user-info-button/user-info-button";
+import Card from "./components/card/card";
 
-type tgUser = typeof window.Telegram.WebApp.initDataUnsafe.user;
+const tg = window.Telegram.WebApp;
+type tgUser = typeof tg.initDataUnsafe.user
 const initialUser: tgUser = {
   id: 0,
   first_name: "default name",
   last_name: "default last name",
   username: "defaultusername",
-};
+}
 
 const App: React.FC = () => {
   const [user, setUser] = useState<typeof initialUser>(initialUser);
-  // const [points, setPoints] = useState(0);
-  const [log, setLog] = useState<string>("Initializing...");
+  const [points, setPoints] = useState(0);
 
-  // const addPoints = () => setPoints(points + 1);
+  const addPoints = () => setPoints(points + 1);
 
   useEffect(() => {
-    try {
-      if (!window.Telegram || !window.Telegram.WebApp) {
-        alert(`Telegram WebApp is not available. \n Window Telegram: ${JSON.stringify(window.Telegram)}`);
-        setLog(
-          `Telegram WebApp is not available. \n Window Telegram: ${JSON.stringify(
-            window.Telegram
-          )}`
-        );
-        return;
-      }
-      const tg = window.Telegram.WebApp;
-      tg.ready();
-      setLog(
-        `Telegram WebApp is initialized successfully.\n Window Telegram: ${JSON.stringify(
-          window.Telegram
-        )}`
-      );
-      // Получение данных пользователя
-      const user = tg.initDataUnsafe?.user;
-      setUser(user ? user : initialUser);
-    } catch (error) {
-      setLog(
-        `Error initializing Telegram WebApp: Error: ${error} \n Window Telegram: ${JSON.stringify(
-          window.Telegram
-        )}`
-      );
-    }
+    // Инициализация приложения
+    tg.ready();
+
+    // Получение данных пользователя
+    const user = tg.initDataUnsafe?.user;
+    setUser(user ? user : initialUser);
   }, []);
 
   return (
     <div className="p-4">
-      {log}
-      user {JSON.stringify(user)}
-    </div>
-  );
-};
-
-export default App;
-
-
-{/* <header className="p-4 bg-gray-800 text-white flex justify-between">
+      <header className="p-4 bg-gray-800 text-white flex justify-between">
         <span>Баллы: {points} 💰</span>
         <button
           className="bg-yellow-500 text-black px-4 py-2 rounded-lg"
@@ -70,4 +40,9 @@ export default App;
       <h1 className="text-2xl font-bold">Привет, {user.first_name}!</h1>
       <p className="text-gray-600">Добро пожаловать в Mini App для Telegram.</p>
       <UserInfoButton />
-      <Card /> */}
+      <Card />
+    </div>
+  );
+};
+
+export default App;
