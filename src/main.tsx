ed0f916322d942cdb35f37b441784e809ffdfa77
Thomas Weaverson <thomasweaverson@gmail.com>
2025-01-27 12:44:58 +0300

    1.1.5 once more weather widget

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { setupTelegram } from "./setup-telegram.ts"
 
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { setupTelegram } from "./setup-telegram.ts"
 setupTelegram();
 
 createRoot(document.getElementById("root")!).render(
