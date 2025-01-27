d2e2297205b312b37e7995f52015b0b8e467dc4c
Thomas Weaverson <thomasweaverson@gmail.com>
2025-01-27 16:22:48 +0300

    Revert "1.1.5 once more weather widget"
    This reverts commit ed0f916322d942cdb35f37b441784e809ffdfa77.

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-
-import dotenv from 'dotenv';
-dotenv.config();
-
 import { setupTelegram } from "./setup-telegram.ts"
+
 setupTelegram();
 
 createRoot(document.getElementById("root")!).render(
