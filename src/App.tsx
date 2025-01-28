0c50160cab59f524a2a0bcb9a541eb2b58bb38ef
Thomas Weaverson <thomasweaverson@gmail.com>
2025-01-28 13:35:29 +0300

    2.0.0 release production bI

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Header from "./components/header/header";
 import Greetings from "./components/greetings/greetings";
 import CatImageFetcher from "./components/cat-image-fetcher/cat-image-fetcher";
 import { UserContextProvider } from "./context/user-context-provider";
-import DebugContainer from "./components/debug-container/debug-container";
+// import DebugContainer from "./components/debug-container/debug-container";
 
 function App(): JSX.Element {
   return (
@@ -13,7 +13,7 @@ function App(): JSX.Element {
         <CatImageFetcher />
       </main>
       <footer className="p-1">
-        <DebugContainer />
+        {/* <DebugContainer /> */}
       </footer>
     </UserContextProvider>
   );
