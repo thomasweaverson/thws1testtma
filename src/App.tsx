4e476145b517c426593b53ea014c759809867368
Thomas Weaverson <thomasweaverson@gmail.com>
2025-01-28 16:44:29 +0300

    2.0.6 patch heder, positioning and Greetings

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,27 @@ import Header from "./components/header/header";
 import Greetings from "./components/greetings/greetings";
 import CatImageFetcher from "./components/cat-image-fetcher/cat-image-fetcher";
 import { UserContextProvider } from "./context/user-context-provider";
-// import DebugContainer from "./components/debug-container/debug-container";
 
 function App(): JSX.Element {
   return (
     <UserContextProvider>
       <Header />
-      <main className="p-1">
+      <main className="h-screen flex flex-col justify-center items-center p-1 relative">
+        <div className="absolute top-0 left-0 w-full h-[60px]"></div>{" "}
+        {/* Занимает место для Header */}
         <Greetings />
         <CatImageFetcher />
       </main>
-      {/* <footer className="p-1">
-        <DebugContainer />
-      </footer> */}
+      <footer className="p-1">@thws 2025</footer>
     </UserContextProvider>
   );
 }
 
 export default App;
+
+// import DebugContainer from "./components/debug-container/debug-container";
+{
+  /* <footer className="p-1">
+        <DebugContainer />
+      </footer> */
+}
--- src/components/greetings/greetings.tsx
+++ src/components/greetings/greetings.tsx
@@ -3,7 +3,6 @@ import { useUserContext } from "../../context/use-user-context";
 
 function Greetings(): JSX.Element | null {
   const [isVisible, setIsVisible] = useState(true);
-
   const { user } = useUserContext();
 
   const handleClose = () => {
@@ -16,7 +15,7 @@ function Greetings(): JSX.Element | null {
 
   return (
     <section
-      className={`w-full bg-slate-50 shadow-md p-3 flex items-center justify-between gap-2 relative transition-opacity duration-500 ${
+      className={`fixed bottom-0 left-0 right-0 bg-slate-50 shadow-md p-3 flex items-center justify-between gap-2 transition-opacity duration-500 z-50${
         isVisible ? "opacity-100" : "opacity-0"
       }`}
     >
@@ -31,19 +30,16 @@ function Greetings(): JSX.Element | null {
       >
         ✕
       </button>
-
       {/* Изображение котика */}
       <img
         src="/img/hicat.png"
         alt="Greeting Cat"
         className="rounded-full object-cover w-16 h-16 transition-all duration-300"
       />
-
       {/* Облачко с текстом */}
       <div className="relative bg-blue-100 p-3 rounded-lg shadow-md max-w-md transition-all duration-300 mr-5">
         {/* Хвостик облачка */}
         <div className="absolute -left-2 top-1/2 transform -translate-y-1/2 w-0 h-0 border-t-8 border-t-transparent border-r-8 border-r-blue-100 border-b-8 border-b-transparent"></div>
-
         {/* Текст приветствия */}
         <p className="text-gray-800 text-sm">
           Hi, <span className="font-semibold">{user?.first_name}</span>{" "}
